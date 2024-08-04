98ac08f7d7c535e3d69fda49bf37724e46944e4d
VisionDeCreator <yusuke94@proton.me>
2024-08-04 15:36:38 +0900

    Update to support payments to protocol at mint

--- src/interface/configInterface.ts
+++ src/interface/configInterface.ts
@@ -3,6 +3,7 @@ export interface NetworkConfig {
     packageId: string;
     coolerFactoryId: string;
     transferPolicyId?: string; // This is optional since it's not in devnet and mainnet
+    factorySettingsId?: string; 
 }
 
 export interface ConfigInterface {
